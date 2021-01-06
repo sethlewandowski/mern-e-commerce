4152ae029b6818a5b14ac77d828335c8eb153fb5
sethl <lewandowski.seth@gmail.com>
2021-01-06 13:32:37 -0700

    saving work finished section 2 lesson 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to E Commerce</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <HomeScreen />
+        </main>
+      </Container>
+      <Footer />
     </>
   )
 }
