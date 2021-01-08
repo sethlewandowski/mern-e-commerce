49a613267cdaf7ea4495273b66b1c38181ec6996
sethl <lewandowski.seth@gmail.com>
2021-01-08 12:59:19 -0700

    finished section 7

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,22 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 )
+
+// method to be used in the controller to make sure passwords match
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// add middleware to encrypt password before saving to db
+userSchema.pre('save', async function (next) {
+  // only hash password if it's been changed. not if any change has been made to the user
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model('User', userSchema)
 
