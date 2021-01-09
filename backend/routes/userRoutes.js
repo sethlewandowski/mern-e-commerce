c4de0864676c7124b995edaa75fa949d0304e70b
sethl <lewandowski.seth@gmail.com>
2021-01-09 09:22:09 -0700

    finished section 8

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile) // runs req thru auth middleware
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile) // runs req thru auth middleware
 
 export default router
---
+++ frontend/src/screens/ProfileScreen.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+
+const ProfileScreen = ({ location, history }) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [message, setMessage] = useState(null)
+
+  const dispatch = useDispatch()
+
+  const userDetails = useSelector((state) => state.userDetails)
+  const { loading, error, user } = userDetails
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
+
+  useEffect(() => {
+    if (!userInfo) {
+      // not logged in
+      history.push('/login')
+    } else {
+      if (!user.name) {
+        // if logged in but state doesn't have user info, we hit the profile endpoint which populates our state with user info based on id from userLogin state
+        dispatch(getUserDetails('profile'))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+      }
+    }
+  }, [dispatch, history, userInfo, user]) // page will update based on changes in any of these values
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (password !== confirmPassword) {
+      setMessage('Passwords do not match')
+    } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
+    }
+  }
+
+  return (
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+        {error && <Message variant="danger">{error}</Message>}
+        {message && <Message variant="danger">{message}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="you"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="you@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="XXXXXXXXX"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="XXXXXXXX"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Update Profile
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
